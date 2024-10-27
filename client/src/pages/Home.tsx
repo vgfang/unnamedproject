78cd25095c4af2d7602307469dee25fac63939af
vgfang <vgfang@yandex.com>
2024-10-27 16:46:19 -0700

    protected route works

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,18 +1,26 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import * as apiService from "../services/apiService";
 import * as toastService from "../services/toastService";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   useEffect(() => {
     const fetchData = async () => {
-      const data = await apiService.getTest();
-      toastService.info("hello");
-      console.log(data);
-      return data;
+      try {
+        const data = await apiService.getProtected();
+        toastService.success("Successfully logged in");
+        console.log(data);
+      } catch (error) {
+        toastService.error("Error validating auth token");
+        return navigate("/login");
+      }
     };
+
     fetchData();
-  }, []);
+  }, [navigate]);
   return (
     <>
       <h1>Home</h1>
--- client/src/services/apiService.ts
+++ client/src/services/apiService.ts
@@ -5,6 +5,19 @@ const api = axios.create({
   baseURL: "/api", // Backend base URL
 });
 
+// add a request interceptor for jwt authenication
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("jwtAccess");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 export const getTest = async () => {
   try {
     const response = await api.get("test");
@@ -18,6 +31,15 @@ export const loginWithDiscord = async (code: string, redirectURI: string) => {
   try {
     const body = { code: code, redirectURI: redirectURI };
     return await api.post("auth/login-discord", body);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getProtected = async () => {
+  try {
+    const response = await api.get("protected");
+    return response.data;
   } catch (error) {
     throw error;
   }
