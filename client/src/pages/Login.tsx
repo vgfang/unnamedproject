b9978bf9af22d83bd33d9a2e7aa579a38e8dcede
vgfang <vgfang@yandex.com>
2024-11-11 19:49:30 -0800

    starting to implement standard login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,19 +1,111 @@
-const Login: React.FC = () => {
+import { FormEvent, useState } from "react";
+
+const Login = () => {
+  const [isLoggingIn, setIsLoggingIn] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleDiscordLogin = () => {
     window.location.href = "/auth/discord"; // Redirect to Discord OAuth route
   };
 
+  const handleEmailPasswordFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+      isLoggingIn: isLoggingIn,
+    };
+
+    console.log(data);
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
-        <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
-        <button
-          onClick={handleDiscordLogin}
-          className="bg-indigo-600 text-white px-4 py-2 rounded-md w-full hover:bg-indigo-500"
-        >
-          Login with Discord
-        </button>
+    <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-1">
+        <h1 className="text-2xl">ghost lounge xyz</h1>
+        <h1 className="text-2xl">[ ⩀☕ ]</h1>
       </div>
+      <p className="">but all i feel is blame</p>
+      <br />
+      <span className="flex my-4 justify-center">
+        <a
+          onClick={() => setIsLoggingIn(true)}
+          className={`text-xl px-2 py-1 cursor-pointer transition-colors duration-200 ${
+            isLoggingIn
+              ? "bg-black text-white font-bold"
+              : "bg-transparent text-gray-500"
+          }`}
+        >
+          login
+        </a>
+        <a
+          onClick={() => setIsLoggingIn(false)}
+          className={`text-xl px-2 py-1 cursor-pointer transition-colors duration-200 ${
+            !isLoggingIn
+              ? "bg-black text-white font-bold"
+              : "bg-transparent text-gray-500"
+          }`}
+        >
+          regis
+        </a>
+      </span>
+      <form id="email-password-form" onSubmit={handleEmailPasswordFormSubmit}>
+        <table className="my-4">
+          <tbody>
+            <tr>
+              <td>
+                <label>email: </label>
+              </td>
+              <td>
+                <input
+                  className="border-black border border-collapse"
+                  type="email"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>passw: </label>
+              </td>
+              <td>
+                <input
+                  className="border-black border border-collapse"
+                  type="password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+      <button
+        form="email-password-form"
+        className="bg-black text-white px-4 py-2 rounded-md my-2"
+      >
+        {isLoggingIn && "login "}
+        {!isLoggingIn && "register "}
+        w/ email
+      </button>
+      <span className="">- or -</span>
+      <button
+        onClick={handleDiscordLogin}
+        className="bg-indigo-600 text-white px-4 py-2 rounded-md w-full hover:bg-indigo-500 my-2"
+      >
+        {isLoggingIn && "login "}
+        {!isLoggingIn && "register "}
+        w/ discord
+      </button>
+      <br />
+      {isLoggingIn && <a>forgot password?</a>}
+      {!isLoggingIn && <span>\|^__^|/</span>}
     </div>
   );
 };
--- client/src/services/apiService.ts
+++ client/src/services/apiService.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
+
+import * as ToastService from "./toastService";
+
 import { type User } from "../../../server/models/user";
 
 const api = axios.create({
   baseURL: "/api", // Backend base URL
 });
 
-// add a request interceptor for jwt authenication
+// add a request interceptor for adding jwt authenication if in localstorage
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("jwtAccess");
@@ -18,29 +21,43 @@ api.interceptors.request.use(
     return Promise.reject(error);
   },
 );
+
+// showing toast response
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      // server responded with a status code outside of 2xx
+      ToastService.error(
+        error.response.data?.message || "Something went wrong",
+      );
+    } else if (error.request) {
+      ToastService.error("Network error. Please try again later");
+    } else {
+      ToastService.error("An unexpected error occurred");
+    }
+
+    return Promise.reject(error); // continue rejecting the promise
+  },
+);
+
 export const getTest = async () => {
-  try {
-    const response = await api.get("test");
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get("test");
+  return response.data;
 };
 
 export const loginWithDiscord = async (code: string, redirectURI: string) => {
-  try {
-    const body = { code: code, redirectURI: redirectURI };
-    return await api.post("auth/login-discord", body);
-  } catch (error) {
-    throw error;
-  }
+  const body = { code: code, redirectURI: redirectURI };
+  return await api.post("auth/login-discord", body);
 };
 
 export const getProtected = async () => {
-  try {
-    const response = await api.get("protected");
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await api.get("protected");
+  return response.data;
 };
+
+export const registerOrLoginUser = async (
+  email: string,
+  password: string,
+  isLoggingIn: boolean,
+) => {};
